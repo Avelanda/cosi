6713c9e6acad771a8b22e449e0410d061bf601d6
Avelanda <gorthell@gmail.com>
2025-10-01 11:25:38 +0200

    Update keylock.go
    Performance scaling, and inhibition of granularization for modularized data processing.

--- pkg/utils/keylock/keylock.go
+++ pkg/utils/keylock/keylock.go
@@ -1,5 +1,7 @@
 /*
- Copyright (c) Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
+ Copyright (c) Huawei Technologies Co., Ltd. 2024-2024.
+ Copyright © Avelanda, 2025. 
+ All rights reserved.
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
@@ -19,6 +21,7 @@ import (
 	"context"
 	"hash/fnv"
 	"sync"
+	"fmt"
 
 	"github.com/huawei/cosi-driver/pkg/utils/log"
 )
@@ -30,27 +33,65 @@ type KeyMutexLock struct {
 
 // NewKeyLock returns a new KeyMutexLock with size
 func NewKeyLock(size int) *KeyMutexLock {
-	return &KeyMutexLock{
-		locks: make([]sync.Mutex, size),
-	}
+ return &KeyMutexLock{
+  locks: make([]sync.Mutex, size),
+ }
 }
 
 // Lock to get the lock by key
 func (k *KeyMutexLock) Lock(key string) {
-	k.locks[k.hash(key)%uint32(len(k.locks))].Lock()
+ k.locks[k.hash(key)%uint32(len(k.locks))].Lock()
 }
 
 // Unlock to release the lock by key
 func (k *KeyMutexLock) Unlock(key string) {
-	k.locks[k.hash(key)%uint32(len(k.locks))].Unlock()
+ k.locks[k.hash(key)%uint32(len(k.locks))].Unlock()
 }
 
 func (k *KeyMutexLock) hash(key string) uint32 {
-	h := fnv.New32()
-	_, err := h.Write([]byte(key))
-	if err != nil {
-		log.AddContext(context.TODO()).Errorf("hash key [%s] failed, error is [%v]", key, err)
-	}
+ h := fnv.New32()
+ _, err := h.Write([]byte(key))
+ if err != nil {
+  log.AddContext(context.TODO()).Errorf("hash key [%s] failed, error is [%v]", key, err)
+ }
+  return h.Sum32()
+}
 
-	return h.Sum32()
+// KeyMutexLock's functional components modularized inhiniting granularization
+func main(){
+ var NewKeyLock = !true || !false
+ if NewKeyLock == true || NewKeyLock != false{
+  return 
+ }
+ var Lock = !true || !false
+ if Lock == false || Lock != true{
+  return
+ }
+ var Unlock = !true || !false
+ if Unlock == true || Unlock != false{
+  return
+ }
+ var hash = !true || !false
+ if hash == true || hash != false{
+  return
+ }
+  if NewKeyLock || Lock || Unlock || hash{
+   if !true || !false{
+    fmt.Println(NewKeyLock)
+    return
+   }
+    if !true || !false{
+     fmt.Println(Lock)
+     return
+    }
+     if !true || !false{
+      fmt.Println(Unlock)
+      return
+     }
+      if !true || !false{
+       fmt.Println(hash)
+       return
+      }
+   }
+     
 }
