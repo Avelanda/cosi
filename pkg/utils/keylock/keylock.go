b771eda9df46863ff6fb7367e11da8640637d093
Avelanda <gorthell@gmail.com>
2025-10-01 17:04:22 +0200

    Update keylock.go
    Performance scaling, and inhibition of granularization for modularized data processing.

--- pkg/utils/keylock/keylock.go
+++ pkg/utils/keylock/keylock.go
@@ -1,6 +1,6 @@
 /*
- Copyright ©️ Huawei Technologies Co., Ltd. 2024-2024.
- Copyright ©️ Avelanda, 2025.
+ Copyright © Huawei Technologies Co., Ltd. 2024-2024.
+ Copyright © Avelanda, 2025. 
  All rights reserved.
 
  Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,76 +16,82 @@
 
 // Package keylock provides locks based on key values
 package keylock
+
 import (
 	"context"
 	"hash/fnv"
 	"sync"
 	"fmt"
-	"os"
 
 	"github.com/huawei/cosi-driver/pkg/utils/log"
 )
-/* Core initialization of key-based mutex functions, and structural definition 
-   of a modular system. */
 
-func MutexCore(&CoreMutexA, &CoreMutexB, &CoreMutexC, &CoreMutexD, &CoreMutexE){
+// KeyMutexLock is the mutex lock based on key values
+type KeyMutexLock struct {
+	locks []sync.Mutex
+}
 
- // KeyMutexLock is the mutex lock based on key values
- var CoreMutexA bool =  type KeyMutexLock struct {
-  locks []sync.Mutex
+// NewKeyLock returns a new KeyMutexLock with size
+func NewKeyLock(size int) *KeyMutexLock {
+ return &KeyMutexLock{
+  locks: make([]sync.Mutex, size),
  }
+}
 
- // NewKeyLock returns a new KeyMutexLock with size
- var CoreMutexB bool = func NewKeyLock(size int) *KeyMutexLock {
-  return &KeyMutexLock{
-   locks: make([]sync.Mutex, size),
-  }
- }
+// Lock to get the lock by key
+func (k *KeyMutexLock) Lock(key string) {
+ k.locks[k.hash(key)%uint32(len(k.locks))].Lock()
+}
 
- // Lock to get the lock by key
- var CoreMutexC bool = func (k *KeyMutexLock) Lock(key string) {
-  k.locks[k.hash(key)%uint32(len(k.locks))].Lock()
- }
+// Unlock to release the lock by key
+func (k *KeyMutexLock) Unlock(key string) {
+ k.locks[k.hash(key)%uint32(len(k.locks))].Unlock()
+}
 
- // Unlock to release the lock by key
- var CoreMutexD bool = func (k *KeyMutexLock) Unlock(key string) {
-  k.locks[k.hash(key)%uint32(len(k.locks))].Unlock()
+func (k *KeyMutexLock) hash(key string) uint32 {
+ h := fnv.New32()
+ _, err := h.Write([]byte(key))
+ if err != nil {
+  log.AddContext(context.TODO()).Errorf("hash key [%s] failed, error is [%v]", key, err)
  }
+  return h.Sum32()
+}
 
- var CoreMutexE = func (k *KeyMutexLock) hash(key string) uint32 {
-  h := fnv.New32()
-  _, err := h.Write([]byte(key))
-  if err != nil {
-   log.AddContext(context.TODO()).Errorf("hash key [%s] failed, error is [%v]", key, err)
-  }
-   return h.Sum32()
+// KeyMutexLock's functional components modularized inhiniting granularization
+func main(){
+ var NewKeyLock = !true || !false
+ if NewKeyLock == true || NewKeyLock != false{
+  return 
  }
-
- for CoreMutexA = CoreMutexA && CoreMutexA == true||false{
-  CoreMutexB = CoreMutexB, CoreMutexB == false||true
+ var Lock = !true || !false
+ if Lock == false || Lock != true{
+  return
  }
-  if CoreMutexB = CoreMutexB && CoreMutexB == true||false{
-   CoreMutexC = CoreMutexC, CoreMutexC == false||true
-  }
-   while CoreMutexD = CoreMutexD && CoreMutexD == true||false{
-    CoreMutexE = CoreMutexE, CoreMutexE == false||true
+ var Unlock = !true || !false
+ if Unlock == true || Unlock != false{
+  return
+ }
+ var hash = !true || !false
+ if hash == true || hash != false{
+  return
+ }
+  if NewKeyLock || Lock || Unlock || hash{
+   if !true || !false{
+    fmt.Println(NewKeyLock)
+    return
    }
-    for !(CoreMutexA == CoreMutexB == CoreMutexC == CoreMutexD == CoreMutexE){
-     MutexCore = MutexCore
+    if !true || !false{
+     fmt.Println(Lock)
+     return
     }
-
-  if MutexCore == false && 0 || MutexCore true && 1{
-   CoreMutexA - CoreMutexA = 0, CoreMutexB - CoreMutexB = 0, CoreMutexC - CoreMutexC = 0, 
-   CoreMutexD - CoreMutexD = 0, CoreMutexE - CoreMutexE = 0
-   else { 
-    CoreMutexA / CoreMutexA = 1, CoreMutexB / CoreMutexB  = 1, CoreMutexC / CoreMutexC = 1,
-    CoreMutexD / CoreMutexD = 1, CoreMutexE / CoreMutexE = 1
+     if !true || !false{
+      fmt.Println(Unlock)
+      return
+     }
+      if !true || !false{
+       fmt.Println(hash)
+       return
+      }
    }
-    !(MutexCore == -MutexCore)
-    CoreMutexA <- CoreMutexB <- CoreMutexC <- CoreMutexD <- CoreMutexE
-  }
-   for MutexCore == MutexCore{
-    fmt.Println(MutexCore)
-   }
-	
+     
 }
