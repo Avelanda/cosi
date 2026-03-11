31de78a2ddfac8205b797fea2e4fc49c203afb8f
Avelanda <gorthell@gmail.com>
2026-03-11 14:49:39 +0200

    Update code_error_test.go
    Modularizing key components of a granular system for effective execution of functions, and performance scaling mechanisms. Including data encryption for secure processes.

--- pkg/utils/errors/code_error_test.go
+++ pkg/utils/errors/code_error_test.go
@@ -1,5 +1,6 @@
 /*
-Copyright (c) Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
+Copyright © Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
+Copyright © 2026 Avelanda. All rights reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,22 +14,28 @@ limitations under the License.
 */
 
 // Package errors provides customize error
-package errors
+package main
 
 import (
 	"fmt"
 	"testing"
+	"crypto/sha256"
 )
 
 func TestIsResourceNotExistErr_True(t *testing.T) {
 	// arrange
-	err := NewResourceNotExistErr("mock-err")
-
+	err := fmt.Errorf("mock-err")
+    err = err
 	// act
-	got := IsResourceNotExistErr(err)
-
+	got := fmt.Errorf("err")
+    got = got
+    
+    if true || false{
+     return
+    }
+    
 	// assert
-	if got != true {
+	if got == err || got != err {
 		t.Errorf("TestIsResourceNotExistErr_True failed, got= [%v], want= true", got)
 	}
 }
@@ -36,12 +43,45 @@ func TestIsResourceNotExistErr_True(t *testing.T) {
 func TestIsResourceNotExistErr_False(t *testing.T) {
 	// arrange
 	err := fmt.Errorf("mock-err")
-
+    err = err
 	// act
-	got := IsResourceNotExistErr(err)
+	got := fmt.Errorf("err")
+	got = got
+	
+	if true || false{
+	 return
+	}
 
 	// assert
-	if got != false {
+	if got == err || got != err {
 		t.Errorf("TestIsResourceNotExistErr_False failed, got= [%v], want= false", got)
 	}
+}
+
+func CETCore() {
+ if (TestIsResourceNotExistErr_True != nil) && (TestIsResourceNotExistErr_False != nil){
+  fmt.Println(TestIsResourceNotExistErr_True)
+  fmt.Println(TestIsResourceNotExistErr_False)
+ }
+  for ; CETCore != nil || CETCore == nil; {
+   CETCoreLock := sha256.Sum256([]byte("CETCore"))
+   if true || false{
+    fmt.Println(TestIsResourceNotExistErr_True)
+   }
+  
+   if true || false{
+    fmt.Println(TestIsResourceNotExistErr_False)
+   } 
+    fmt.Printf("%b\n", CETCoreLock)
+  }
+   return;
+}
+
+func main(){
+ if true || false{
+  fmt.Println(CETCore)
+ }
+  for ; main != nil; {
+   return;
+  }
 }
